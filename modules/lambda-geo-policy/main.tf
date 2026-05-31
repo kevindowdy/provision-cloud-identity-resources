594a7a9488c72f6780228cc804d41e14df429033
kd <kdowdy.noreply@gmail.com>
2026-05-30 23:52:50 -0400

    remove unnecessary actions in geo policy

--- modules/lambda-geo-policy/main.tf
+++ modules/lambda-geo-policy/main.tf
@@ -4,36 +4,7 @@ data "aws_iam_policy_document" "this" {
     effect = "Allow"
 
     actions = [
-      "geo:ListDevicePositions",
-      "geo:GetDevicePosition",
-      "geo:CalculateRoute",
-      "geo-routes:*",
-      "geo:DescribePlaceIndex",
-      "geo:DescribeGeofenceCollection",
-      "geo:SearchPlaceIndexForPosition",
-      "geo:GetPlace",
-      "geo:ListTrackerConsumers",
-      "geo-places:*",
       "geo:*",
-      "geo:GetMapTile",
-      "geo:ForecastGeofenceEvents",
-      "geo:SearchPlaceIndexForText",
-      "geo:DescribeKey",
-      "geo:VerifyDevicePosition",
-      "geo:DescribeMap",
-      "geo:BatchGetDevicePosition",
-      "geo:GetMapGlyphs",
-      "geo:ListTagsForResource",
-      "geo:GetDevicePositionHistory",
-      "geo:DescribeTracker",
-      "geo:GetMapSprites",
-      "geo:GetMapStyleDescriptor",
-      "geo:CalculateRouteMatrix",
-      "geo:ListGeofences",
-      "geo:GetGeofence",
-      "geo-maps:*",
-      "geo:SearchPlaceIndexForSuggestions",
-      "geo:DescribeRouteCalculator",
     ]
 
     resources = ["*"]
